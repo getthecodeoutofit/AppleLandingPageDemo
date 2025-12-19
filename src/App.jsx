5bdc8aefbb2aa69ee1860c44864c520df03b65fb
getthecodeoutofit <ravimahar12321@gmail.com>
2025-12-19 20:11:15 +0530

    added some clicables

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Hero from "./componenets/Hero"
 import NavBar from "./componenets/Navbar"
+import ProductViewer from "./componenets/ProductViewer.jsx"
 
 const App = () =>
 {
     return (<main>
         <NavBar />
         <Hero/>
+        <ProductViewer/>
+
     </main> )
 }
 export default App
