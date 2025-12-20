53ac85f233b3b5bf4c2b6ae5d81a9311df6f993a
getthecodeoutofit <ravimahar12321@gmail.com>
2025-12-20 22:16:18 +0530

    added macbook model

--- src/componenets/ProductViewer.jsx
+++ src/componenets/ProductViewer.jsx
@@ -3,6 +3,8 @@ import clsx from "clsx";
 import useMacbookStore from "../store";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
+import { MacbookModel16 } from "./models/Macbook-16";
+import MacbookModel14 from "./models/Macbook-14";
 
 const ProductViewer = ()=>{
     const {color,scale,setColor,setScale} = useMacbookStore();
@@ -43,8 +45,8 @@ const ProductViewer = ()=>{
             </div>
 
             <Canvas id="canvas" camera={{position:[0,2,5],fov:50,near:0.1,far:100}}>
-                <Box position={[0,0,0]} scale={10*scale} material-color = {color}>
-                </Box>
+                <ambientLight intensity={2}></ambientLight>
+                <MacbookModel14 scale={0.06} position={[0,0,0]}/>
                 <OrbitControls enableZoom={false}>
 
                 </OrbitControls>
